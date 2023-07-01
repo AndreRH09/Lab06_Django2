85937299bd737400bf55e100b6204a24e49db027
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-07-01 10:09:42 -0500

    migraciones y base de datos

--- proyectoTutorial/proyectoTutorial/settings.py
+++ proyectoTutorial/proyectoTutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'travello.apps.TravelloConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'proyectoTutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
